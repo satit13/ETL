06d2b6bb95bfb26be95043149a5858a6451e324f
= <=>
2017-03-16 14:06:28 +0700

    lab for concurrency - go routine workshop - final code - fix waitgroup problem

--- main.go
+++ main.go
@@ -1,68 +1,62 @@
 package main
 
 import (
-	_ "unicode"
-	"os"
 	"encoding/csv"
+	"fmt"
+	"os"
 	"strconv"
 	"time"
-	"fmt"
+	"sync"
 )
+
+func main() {
+	start := time.Now()
+	
+	extractChannel := make(chan *Order)
+	transformChannel := make(chan *Order)
+	doneChannel := make(chan bool)
+	
+	go extract(extractChannel)
+	fmt.Print("extract finished ")
+	fmt.Println(time.Since(start))
+	go transform(extractChannel, transformChannel)
+	fmt.Print("transform finished ")
+	fmt.Println(time.Since(start))
+	go load(transformChannel, doneChannel)
+	fmt.Print("load finished ")
+	<- doneChannel
+	fmt.Println(time.Since(start))
+}
 
 type Product struct {
 	PartNumber string
-	UnitCost float64
-	UnitPrice float64
+	UnitCost   float64
+	UnitPrice  float64
 }
 
 type Order struct {
 	CustomerNumber int
-	PartNumber string
-	Quantity int
+	PartNumber     string
+	Quantity       int
 
-	UnitCost float64
+	UnitCost  float64 
 	UnitPrice float64
 }
 
-
-func main(){
-	start := time.Now()
-	//ประกาศ channel
-	extractChannel := make(chan *Order)
-	transformChannel := make(chan *Order)
-	doneChannel := make(chan bool)
-
-	//orders := extract()
-	go extract(extractChannel)
-	//orders = transform(orders)
-	go transform(extractChannel,transformChannel)
-	go load(transformChannel, doneChannel)
-	<- doneChannel
-	fmt.Println(time.Since(start))
-
-}
-
-
-func extract(ch chan *Order)  {
-	//result :=  []*Order{}
-
-	f,_ := os.Open("./orders.txt")
+func extract(ch chan *Order) {
+	f, _ := os.Open("./orders.txt")
 	defer f.Close()
-	r:= csv.NewReader(f)
+	r := csv.NewReader(f)
 
-	for record, err := r.Read();err == nil; record, err = r.Read(){
-		fmt.Println("extract ",record)
+	for record, err := r.Read(); err == nil;record, err = r.Read() {
 		order := new(Order)
 		order.CustomerNumber, _ = strconv.Atoi(record[0])
 		order.PartNumber = record[1]
-		order.Quantity ,_ = strconv.Atoi(record[2])
-		//result = append(result, order)
+		order.Quantity, _ = strconv.Atoi(record[2])
 		ch <- order
-
 	}
+	
 	close(ch)
-
-	//return result
 }
 
 func transform(extractChannel, transformChannel chan *Order) {
@@ -70,55 +64,401 @@ func transform(extractChannel, transformChannel chan *Order) {
 	defer f.Close()
 	r := csv.NewReader(f)
 
-	// อ่านข้อมูลสินค้าจาก txt และ สร้าง map เพื่อใช้สำหรับการค้นหา โดยมี index = PartNumber
 	records, _ := r.ReadAll()
 	productList := make(map[string]*Product)
 	for _, record := range records {
-		product :=  new(Product)
+		product := new(Product)
 		product.PartNumber = record[0]
 		product.UnitCost, _ = strconv.ParseFloat(record[1], 64)
-		product.UnitPrice, _  = strconv.ParseFloat(record[2], 64)
+		product.UnitPrice, _ = strconv.ParseFloat(record[2], 64)
 		productList[product.PartNumber] = product
 	}
 
+	//numMessages := 0
+	w := sync.WaitGroup{}
 
-	//for idx, _ := range orders {
-	for o := range extractChannel{
-		//fmt.Println("idx : ",idx)
-		go func(o *Order){
+	for o := range extractChannel {
+		//numMessages++
+		w.Add(1)
+		go func(o *Order) {
+
+			time.Sleep(1 * time.Millisecond)
+			o.UnitCost = productList[o.PartNumber].UnitCost
+			o.UnitPrice = productList[o.PartNumber].UnitPrice
+			transformChannel <- o
+			//numMessages--
+			//fmt.Println("transform message :",numMessages)
+			w.Done()
+		}(o)
+	}
+	w.Wait()
+
+	//
+	//for ;numMessages > 0; {
+	//	time.Sleep(1 * time.Millisecond)
+	//}
+	//
+	close(transformChannel)
+	fmt.Println("close channel transform")
+
+
+}
+
+func load(transformChannel chan *Order, doneChannel chan bool) {
+	f, _ := os.Create("./dest.txt")
+	defer f.Close()
+
+//	numMessages := 0
+//	numRoutine :=0
+
+	fmt.Fprintf(f, "%20s%15s%12s%12s%15s%15s\n",
+		"Part Number", "Quantity",
+		"Unit Cost", "Unit Price",
+		"Total Cost", "Total Price")
+	w := sync.WaitGroup{}
+	for o := range transformChannel {
+		//numMessages++
+		//numRoutine++
+		w.Add(1)
+		go func(o *Order) {
+			time.Sleep(1 * time.Millisecond)
+			//fmt.Println("routine no:",numRoutine)
+			fmt.Fprintf(f, "%20s %15d %12.2f %12.2f %15.2f %15.2f\n",
+				o.PartNumber, o.Quantity,
+				o.UnitCost, o.UnitPrice,
+				o.UnitCost*float64(o.Quantity),
+				o.UnitPrice*float64(o.Quantity))
+			w.Done()
+			//numMessages--
+			//fmt.Println("load message :",numMessages)
+		}(o)
+	}
+	w.Wait()
+	//for ;numMessages > 0; {
+	//	time.Sleep(1 * time.Millisecond)
+	//}
+	doneChannel <- true
+}
+/* 4.6.1 - Intitial ETL with synchronous processing
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	orders := extract()
+	orders = transform(orders)
+	load(orders)
+	fmt.Println(time.Since(start))
+}
+
+type Product struct {
+	PartNumber string
+	UnitCost   float64
+	UnitPrice  float64
+}
+
+type Order struct {
+	CustomerNumber int
+	PartNumber     string
+	Quantity       int
+
+	UnitCost  float64
+	UnitPrice float64
+}
+
+func extract() []*Order {
+	result := []*Order{}
+
+	f, _ := os.Open("./orders.txt")
+	defer f.Close()
+	r := csv.NewReader(f)
+
+	for record, err := r.Read(); err == nil;record, err = r.Read() {
+		order := new(Order)
+		order.CustomerNumber, _ = strconv.Atoi(record[0])
+		order.PartNumber = record[1]
+		order.Quantity, _ = strconv.Atoi(record[2])
+		result = append(result, order)
+	}
+	return result
+}
+
+func transform(orders []*Order) []*Order {
+	f, _ := os.Open("./productList.txt")
+	defer f.Close()
+	r := csv.NewReader(f)
+
+	records, _ := r.ReadAll()
+	productList := make(map[string]*Product)
+	for _, record := range records {
+		product := new(Product)
+		product.PartNumber = record[0]
+		product.UnitCost, _ = strconv.ParseFloat(record[1], 64)
+		product.UnitPrice, _ = strconv.ParseFloat(record[2], 64)
+		productList[product.PartNumber] = product
+	}
+
+	for idx, _ := range orders {
 		time.Sleep(3 * time.Millisecond)
-		//o := orders[idx]
+		o := orders[idx]
+		o.UnitCost = productList[o.PartNumber].UnitCost
+		o.UnitPrice = productList[o.PartNumber].UnitPrice
+	}
+
+	return orders
+}
+
+func load(orders []*Order) {
+	f, _ := os.Create("./dest.txt")
+	defer f.Close()
+
+	fmt.Fprintf(f, "%20s%15s%12s%12s%15s%15s\n",
+		"Part Number", "Quantity",
+		"Unit Cost", "Unit Price",
+		"Total Cost", "Total Price")
+
+	for _, o := range orders {
+		time.Sleep(1 * time.Millisecond)
+		fmt.Fprintf(f, "%20s %15d %12.2f %12.2f %15.2f %15.2f\n",
+			o.PartNumber, o.Quantity,
+			o.UnitCost, o.UnitPrice,
+			o.UnitCost*float64(o.Quantity),
+			o.UnitPrice*float64(o.Quantity))
+	}
+}
+*/
+
+/* 4.6.2 - Change to concurrent processing
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	
+	extractChannel := make(chan *Order)
+	transformChannel := make(chan *Order)
+	doneChannel := make(chan bool)
+	
+	go extract(extractChannel)
+	go transform(extractChannel, transformChannel)
+	go load(transformChannel, doneChannel)
+	
+	<- doneChannel
+	fmt.Println(time.Since(start))
+}
+
+type Product struct {
+	PartNumber string
+	UnitCost   float64
+	UnitPrice  float64
+}
+
+type Order struct {
+	CustomerNumber int
+	PartNumber     string
+	Quantity       int
+
+	UnitCost  float64 
+	UnitPrice float64
+}
+
+func extract(ch chan *Order) {
+	f, _ := os.Open("./orders.txt")
+	defer f.Close()
+	r := csv.NewReader(f)
+
+	for record, err := r.Read(); err == nil;record, err = r.Read() {
+		order := new(Order)
+		order.CustomerNumber, _ = strconv.Atoi(record[0])
+		order.PartNumber = record[1]
+		order.Quantity, _ = strconv.Atoi(record[2])
+		ch <- order
+	}
+	
+	close(ch)
+}
+
+func transform(extractChannel, transformChannel chan *Order) {
+	f, _ := os.Open("./productList.txt")
+	defer f.Close()
+	r := csv.NewReader(f)
+
+	records, _ := r.ReadAll()
+	productList := make(map[string]*Product)
+	for _, record := range records {
+		product := new(Product)
+		product.PartNumber = record[0]
+		product.UnitCost, _ = strconv.ParseFloat(record[1], 64)
+		product.UnitPrice, _ = strconv.ParseFloat(record[2], 64)
+		productList[product.PartNumber] = product
+	}
+
+	for o := range extractChannel {
+		time.Sleep(3 * time.Millisecond)
 		o.UnitCost = productList[o.PartNumber].UnitCost
 		o.UnitPrice = productList[o.PartNumber].UnitPrice
 		transformChannel <- o
 	}
-	}
-	// ปิดประตู ทางเข้า chanel transform
+	
 	close(transformChannel)
-
-
-	//return orders
-// end of transform
 }
 
-
-//func load(orders []*Order) {
-func load(transformChannel chan *Order,doneChannel chan bool){
+func load(transformChannel chan *Order, doneChannel chan bool) {
 	f, _ := os.Create("./dest.txt")
 	defer f.Close()
 
-	fmt.Fprintf(f, "%20s%15s%12s%12s%15s%15%s",
-		"Part Number", "Quantity", "Unit Cost",
-		"Unit Price", "Total Cost", "Total Price")
+	fmt.Fprintf(f, "%20s%15s%12s%12s%15s%15s\n",
+		"Part Number", "Quantity",
+		"Unit Cost", "Unit Price",
+		"Total Cost", "Total Price")
 
-	//for _, o := range orders {
-	for o:= range transformChannel{
+	for o := range transformChannel {
 		time.Sleep(1 * time.Millisecond)
 		fmt.Fprintf(f, "%20s %15d %12.2f %12.2f %15.2f %15.2f\n",
-		o.PartNumber, o.Quantity, o.UnitCost, o.UnitPrice ,
-		o.UnitCost * float64(o.Quantity),
-		o.UnitPrice * float64(o.Quantity))
+			o.PartNumber, o.Quantity,
+			o.UnitCost, o.UnitPrice,
+			o.UnitCost*float64(o.Quantity),
+			o.UnitPrice*float64(o.Quantity))
 	}
-	// ส่งข้อมูล ทำงานสำเร็จ เข้า channel
+	
 	doneChannel <- true
+	
 }
+*/
+
+/* 4.6.3 - Generate independent goroutines for extract and load steps
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	
+	extractChannel := make(chan *Order)
+	transformChannel := make(chan *Order)
+	doneChannel := make(chan bool)
+	
+	go extract(extractChannel)
+	go transform(extractChannel, transformChannel)
+	go load(transformChannel, doneChannel)
+	
+	<- doneChannel
+	fmt.Println(time.Since(start))
+}
+
+type Product struct {
+	PartNumber string
+	UnitCost   float64
+	UnitPrice  float64
+}
+
+type Order struct {
+	CustomerNumber int
+	PartNumber     string
+	Quantity       int
+
+	UnitCost  float64 
+	UnitPrice float64
+}
+
+func extract(ch chan *Order) {
+	f, _ := os.Open("./orders.txt")
+	defer f.Close()
+	r := csv.NewReader(f)
+
+	for record, err := r.Read(); err == nil;record, err = r.Read() {
+		order := new(Order)
+		order.CustomerNumber, _ = strconv.Atoi(record[0])
+		order.PartNumber = record[1]
+		order.Quantity, _ = strconv.Atoi(record[2])
+		ch <- order
+	}
+	
+	close(ch)
+}
+
+func transform(extractChannel, transformChannel chan *Order) {
+	f, _ := os.Open("./productList.txt")
+	defer f.Close()
+	r := csv.NewReader(f)
+
+	records, _ := r.ReadAll()
+	productList := make(map[string]*Product)
+	for _, record := range records {
+		product := new(Product)
+		product.PartNumber = record[0]
+		product.UnitCost, _ = strconv.ParseFloat(record[1], 64)
+		product.UnitPrice, _ = strconv.ParseFloat(record[2], 64)
+		productList[product.PartNumber] = product
+	}
+
+	numMessages := 0
+
+	for o := range extractChannel {
+		numMessages++
+		go func(o *Order) {
+			time.Sleep(3 * time.Millisecond)
+			o.UnitCost = productList[o.PartNumber].UnitCost
+			o.UnitPrice = productList[o.PartNumber].UnitPrice
+			transformChannel <- o
+			numMessages--
+		}(o)
+	}
+	
+	for ;numMessages > 0; {
+		time.Sleep(1 * time.Millisecond)
+	}
+	
+	close(transformChannel)
+}
+
+func load(transformChannel chan *Order, doneChannel chan bool) {
+	f, _ := os.Create("./dest.txt")
+	defer f.Close()
+
+	numMessages := 0
+
+	fmt.Fprintf(f, "%20s%15s%12s%12s%15s%15s\n",
+		"Part Number", "Quantity",
+		"Unit Cost", "Unit Price",
+		"Total Cost", "Total Price")
+
+	for o := range transformChannel {
+		numMessages++
+		go func(o *Order) {
+			time.Sleep(1 * time.Millisecond)
+			fmt.Fprintf(f, "%20s %15d %12.2f %12.2f %15.2f %15.2f\n",
+				o.PartNumber, o.Quantity,
+				o.UnitCost, o.UnitPrice,
+				o.UnitCost*float64(o.Quantity),
+				o.UnitPrice*float64(o.Quantity))
+			numMessages--
+		}(o)
+	}
+	for ;numMessages > 0; {
+		time.Sleep(1 * time.Millisecond)
+	}
+	doneChannel <- true
+	
+}
+*/

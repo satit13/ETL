ee8b5c2aee1c76ccb908f0339685b3293f759737
= <=>
2017-03-16 12:24:45 +0700

    lab for concurrency - go routine workshop - startup code

---
+++ main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	_ "unicode"
+	"os"
+	"encoding/csv"
+	"strconv"
+	"time"
+	"fmt"
+)
+
+type Product struct {
+	PartNumber string
+	UnitCost float64
+	UnitPrice float64
+}
+
+type Order struct {
+	CustomerNumber int
+	PartNumber string
+	Quantity int
+
+	UnitCost float64
+	UnitPrice float64
+}
+
+
+func main(){
+	start := time.Now()
+	//ประกาศ channel
+	extractChannel := make(chan *Order)
+	transformChannel := make(chan *Order)
+	doneChannel := make(chan bool)
+
+	//orders := extract()
+	go extract(extractChannel)
+	//orders = transform(orders)
+	go transform(extractChannel,transformChannel)
+	go load(transformChannel, doneChannel)
+	<- doneChannel
+	fmt.Println(time.Since(start))
+
+}
+
+
+func extract(ch chan *Order)  {
+	//result :=  []*Order{}
+
+	f,_ := os.Open("./orders.txt")
+	defer f.Close()
+	r:= csv.NewReader(f)
+
+	for record, err := r.Read();err == nil; record, err = r.Read(){
+		fmt.Println("extract ",record)
+		order := new(Order)
+		order.CustomerNumber, _ = strconv.Atoi(record[0])
+		order.PartNumber = record[1]
+		order.Quantity ,_ = strconv.Atoi(record[2])
+		//result = append(result, order)
+		ch <- order
+
+	}
+	close(ch)
+
+	//return result
+}
+
+func transform(extractChannel, transformChannel chan *Order) {
+	f, _ := os.Open("./productList.txt")
+	defer f.Close()
+	r := csv.NewReader(f)
+
+	// อ่านข้อมูลสินค้าจาก txt และ สร้าง map เพื่อใช้สำหรับการค้นหา โดยมี index = PartNumber
+	records, _ := r.ReadAll()
+	productList := make(map[string]*Product)
+	for _, record := range records {
+		product :=  new(Product)
+		product.PartNumber = record[0]
+		product.UnitCost, _ = strconv.ParseFloat(record[1], 64)
+		product.UnitPrice, _  = strconv.ParseFloat(record[2], 64)
+		productList[product.PartNumber] = product
+	}
+
+
+	//for idx, _ := range orders {
+	for o := range extractChannel{
+		//fmt.Println("idx : ",idx)
+		go func(o *Order){
+		time.Sleep(3 * time.Millisecond)
+		//o := orders[idx]
+		o.UnitCost = productList[o.PartNumber].UnitCost
+		o.UnitPrice = productList[o.PartNumber].UnitPrice
+		transformChannel <- o
+	}
+	}
+	// ปิดประตู ทางเข้า chanel transform
+	close(transformChannel)
+
+
+	//return orders
+// end of transform
+}
+
+
+//func load(orders []*Order) {
+func load(transformChannel chan *Order,doneChannel chan bool){
+	f, _ := os.Create("./dest.txt")
+	defer f.Close()
+
+	fmt.Fprintf(f, "%20s%15s%12s%12s%15s%15%s",
+		"Part Number", "Quantity", "Unit Cost",
+		"Unit Price", "Total Cost", "Total Price")
+
+	//for _, o := range orders {
+	for o:= range transformChannel{
+		time.Sleep(1 * time.Millisecond)
+		fmt.Fprintf(f, "%20s %15d %12.2f %12.2f %15.2f %15.2f\n",
+		o.PartNumber, o.Quantity, o.UnitCost, o.UnitPrice ,
+		o.UnitCost * float64(o.Quantity),
+		o.UnitPrice * float64(o.Quantity))
+	}
+	// ส่งข้อมูล ทำงานสำเร็จ เข้า channel
+	doneChannel <- true
+}
